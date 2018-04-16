dcc0b02e8d9218d2e5749da1c8ed36e83a079cd3
Juan Carlos Guerra <jguerra@allseasons.com.ar>
2018-04-16 10:35:28 -0300

    Solved Warning Carbon and Work VuetifuJS, everything ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <div id="app">
         <v-app>
-            <v-content>
-                <v-container>Hello world</v-container>
-              </v-content>
+            <v-toolbar>
+                <v-toolbar-side-icon></v-toolbar-side-icon>
+                <v-toolbar-title>Title</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-toolbar-items class="hidden-sm-and-down">
+                  <v-btn flat>Link One</v-btn>
+                  <v-btn flat>Link Two</v-btn>
+                  <v-btn flat>Link Three</v-btn>
+                </v-toolbar-items>
+              </v-toolbar>
         </v-app>
     </div>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
